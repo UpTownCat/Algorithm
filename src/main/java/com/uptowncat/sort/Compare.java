1b5c968ca064bbc9de51332eada7f27182408229
xieshiwang <158668658@qq.com>
2018-02-14 01:05:53 +0800

    快速排序

--- src/main/java/com/uptowncat/sort/Compare.java
+++ src/main/java/com/uptowncat/sort/Compare.java
@@ -1,7 +1,54 @@
 package com.uptowncat.sort;
 
+import java.util.Arrays;
+
 /**
  * Created by shiwang.xie on 2018/1/14.
  */
 public class Compare {
+    public static void main(String[] args) {
+        int[] arr = new int[50000];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int)(Math.random() * 10000);
+        }
+        
+        int[] sysCopy = Utils.copy(arr);
+        long sysStart = System.currentTimeMillis();
+        Arrays.sort(sysCopy);
+        long sysEnd = System.currentTimeMillis();
+        System.out.println(String.format("sys sort %d ", sysEnd - sysStart));
+
+        BubbleSort bubbleSort = new BubbleSort();
+        int[] bubbleCopy = Utils.copy(arr);
+        long bubbleStart = System.currentTimeMillis();
+        bubbleSort.sort(bubbleCopy);
+        long bubbleEnd = System.currentTimeMillis();
+        System.out.println(String.format("bubble sort %d", bubbleEnd - bubbleStart));
+        System.out.println("bubble sort equal: " + Utils.equal(sysCopy, bubbleCopy));
+
+        SelectionSort selectionSort = new SelectionSort();
+        int[] selectionCopy = Utils.copy(arr);
+        long selectionStart = System.currentTimeMillis();
+        selectionSort.sort(selectionCopy);
+        long selectionEnd = System.currentTimeMillis();
+        System.out.println(String.format("selection sort %d", selectionEnd - selectionStart));
+        System.out.println("selection sort equal: " + Utils.equal(sysCopy, selectionCopy));
+
+        InsertionSort insertionSort = new InsertionSort();
+        int[] insertionCopy = Utils.copy(arr);
+        long insertionStart = System.currentTimeMillis();
+        insertionSort.sort(insertionCopy);
+        long insertionEnd = System.currentTimeMillis();
+        System.out.println(String.format("insertion sort %d", insertionEnd - insertionStart));
+        System.out.println("insertion sort equal: " + Utils.equal(sysCopy, insertionCopy));
+
+        QuickSort quickSort = new QuickSort();
+        int[] quickCopy = Utils.copy(arr);
+        long quickStart = System.currentTimeMillis();
+        quickSort.sort(quickCopy);
+        long quickEnd = System.currentTimeMillis();
+        System.out.println(String.format("quick sort %d", quickEnd - quickStart));
+        System.out.println("quick sort equal: " + Utils.equal(sysCopy, quickCopy));
+//        System.out.println("origin arr: " + Arrays.toString(arr));
+    }
 }
--- src/main/java/com/uptowncat/sort/Utils.java
+++ src/main/java/com/uptowncat/sort/Utils.java
@@ -1,7 +1,52 @@
 package com.uptowncat.sort;
 
+import java.util.Arrays;
+
 /**
  * Created by shiwang.xie on 2018/1/14.
  */
 public class Utils {
+    private Utils(){}
+
+    /**
+     * 交换数组的两个数
+     * @param arr
+     * @param i
+     * @param j
+     */
+    public static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    /**
+     * 复制数组
+     * @param arr
+     * @return
+     */
+    public static int[] copy(int[] arr) {
+        int[] copyArr = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            copyArr[i] = arr[i];
+        }
+        return copyArr;
+    }
+
+    /**
+     * 判断数组是否相等
+     * @param arr1
+     * @param arr2
+     * @return
+     */
+    public static boolean equal(int[] arr1, int[] arr2) {
+        for (int i = 0; i < arr1.length; i++) {
+            if (arr1[i] != arr2[i]) {
+                System.out.println("arr1: " + Arrays.toString(arr1));
+                System.out.println("arr2: " + Arrays.toString(arr2));
+                return false;
+            }
+        }
+        return true;
+    }
 }
